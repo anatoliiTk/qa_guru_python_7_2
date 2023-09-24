8f09bb2f69fecd5c403354cbfed0a863e83ee84a
atkachh <116884982+atkachh@users.noreply.github.com>
2023-09-24 22:42:34 +0600

    Домашняя работа утрок № 2

--- conftest.py
+++ conftest.py
@@ -1,9 +1,12 @@
 import pytest
+from selene.support.shared import browser
 
-@pytest.fixture(scope="session")
-def user():
-    print("Тестовый пользователь перед тестом")
 
-    yield
-
-    print("Здесь откатываем тестового пользователя")
+@pytest.fixture(scope='function', autouse=True)
+def setup():
+    print("Открыть браузер для тестов")
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    yield browser
+    print("Закрыть браузер")
+    browser.quit()
---
+++ test_urok2_dz.py
@@ -0,0 +1,17 @@
+import pytest
+from selene.support.shared import browser
+from selene import be, have
+
+
+class TestOne:
+    def test_google_should_find_selene(self):
+        browser.open("https://www.google.com/")
+        browser.element('[name="q"]').should(be.blank).type('Hallo World').press_enter()
+        browser.element('[id="result-stats"]').should(have.text('Результатов: примерно'))
+        print('Страница соответствует критериям поиска')
+
+    def test_google_should_not_find_result(self):
+        browser.open("https://www.google.com/")
+        browser.element('[name="q"]').should(be.blank).type('FJE^hds&&Q#^e888190<>').press_enter()
+        browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
+        print('По запросу ничего не найдено')
