9a81841c017a36300ed45bc878eec0206653eba6
atkachh <116884982+atkachh@users.noreply.github.com>
2023-09-25 12:01:16 +0600

    Домашняя работа урок № 2 c правками

--- test_urok2_dz.py
+++ test_urok2_dz.py
@@ -6,8 +6,8 @@ from selene import be, have
 class TestOne:
     def test_google_should_find_selene(self):
         browser.open("https://www.google.com/")
-        browser.element('[name="q"]').should(be.blank).type('Hallo World').press_enter()
-        browser.element('[id="result-stats"]').should(have.text('Результатов: примерно'))
+        browser.element('[name="q"]').should(be.blank).type('Hello, world!').press_enter()
+        browser.element('[class="kb0PBd cvP2Ce"]').should(have.text('Hello, world!'))
         print('Страница соответствует критериям поиска')
 
     def test_google_should_not_find_result(self):
